e621bb2d7bad44cfd6dbfdadb64ad063f102b7d5
tejashwini-793 <tejashwini793@gmail.com>
2025-05-13 15:47:07 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -13,27 +13,7 @@ public class Employee{
         System.out.println("Name: " + name);
         System.out.println("ID: " + id);
         System.out.println("Salary: " + salary);
-        System.out.println("Experience: " + experience);public class Employee{
-    String name;
-    int id;
-    double salary;
-    public Employee(String name,int id,double salary) {
-        this.name = name;
-        this.id = id;
-        this.salary = salary;
-    }
-    public void displayinfo() {
-        System.out.println("Name: " + name);
-        System.out.println("ID: " + id);
-        System.out.println("Salary: " + salary);
-
-    }
-    public static void main(String[] args) {
-        Employee emp1 = new Employee(name:"ABC",id:092,salary:50000.0);
-        emp1.displayinfo();
-    }
-}
-
+        System.out.println("Experience: " + experience);
     }
     public static void main(String[] args) {
         Employee emp1 = new Employee(name:"ABC",id:092,salary:50000.0,experience:3);
